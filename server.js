0552af326afa48effcbd642968b63764a400eaac
student <student@Cjs-MacBook-Pro.local>
2018-12-17 15:54:13 -0500

    flatmap and eventstream npm node package fix for heroku deploy

--- server.js
+++ server.js
@@ -1,37 +1,21 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require('ejs');
+const app = express();
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-
-// parse application/x-www-form-urlencoded
+const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-// Static directory
-app.use(express.static("public"));
+const db = require("./models");
 
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
+require('./routes/html-routes.js')(app, ejs);
+require('./routes/api-routes.js')(app);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
